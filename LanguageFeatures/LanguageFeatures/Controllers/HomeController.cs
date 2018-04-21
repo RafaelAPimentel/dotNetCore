f984971c94d6d644870b779bec6414b5fba70cc5
Rafael Pimentel <rafael.pimentel.ef@outlook.com>
2018-04-21 02:40:05 -0400

    Adding Projects to repo

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() {
+            Dictionary<string, Product> product = new Dictionary<string, Product> {
+                ["Kayak"]= new Product { Name = "Kayak", Price = 275M },
+                ["LifeJacket"] = new Product { Name = "Lifejacket", Price = 48.95M } };
+            
+            return View("Index", product.Keys);
+        }
+    }
+}
---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public bool InStock { get; }
+
+        public Product(bool stock  = true)
+        {
+            InStock = stock;
+        }
+
+        public static Product[] GetProduct() {
+            Product kayak = new Product { Name = "Kayak", Category="Water Craft", Price = 275M };
+            Product lifeJacket = new Product (false){ Name = "LifeJacket", Price = 48.95M };
+
+            kayak.Related = lifeJacket;
+            return new Product[] { kayak, lifeJacket, null };
+        }
+    }
+}
